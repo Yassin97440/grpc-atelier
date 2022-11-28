61dbe5f0bedc590b3567c76e0dcec83b2df1e1ad
Yassin <yassin.abdulla@compufirst.com>
2022-11-28 12:13:12 +0100

    server ok

--- grpc/src/main/java/server/LauncherServer.java
+++ grpc/src/main/java/server/LauncherServer.java
@@ -1,11 +1,25 @@
 package server;
 
+import java.io.IOException;
+
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import server.service.UserServiceImpl;
 
 public class LauncherServer {
   
   public static void main(String[] args) {
-   //ici on va lancer le serveur gRPC
+    //on instancie le serveur 
+    Server server = ServerBuilder
+          .forPort(8080)
+          .addService(new UserServiceImpl()).build();
+
+    try {
+      server.start();
+      server.awaitTermination();
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 }
