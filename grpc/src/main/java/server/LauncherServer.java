a1c6d8535b1c6a94404440ad5ab86779b8dd64f3
Yassin <yassin.abdulla@compufirst.com>
2022-11-28 08:43:17 +0100

    ajout launcher pr server

---
+++ grpc/src/main/java/server/LauncherServer.java
@@ -0,0 +1,11 @@
+package server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+public class LauncherServer {
+  
+  public static void main(String[] args) {
+   //ici on va lancer le serveur gRPC
+  }
+}
